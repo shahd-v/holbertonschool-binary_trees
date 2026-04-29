e0e1402b14b86755a2419e58e5f04b14b374e10c
shahd-v <shahdfaisalr@gmail.com>
2026-04-29 19:02:24 +0300

    task 1 : add a left child

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,22 @@
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
+
+	if (parent->left != NULL)
+	{
+		new->left = parent->left;
+		parent->left->parent = new;
+	}
+
+	parent->left = new;
+	new->parent = parent;
+
+	return (new);
+}
