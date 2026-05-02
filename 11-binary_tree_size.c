90ac91af1453eb4c09d73774a36514f3be1830c5
Sara <sarakhalid.b28@gmail.com>
2026-05-02 21:49:06 +0300

    Clarify parameter documentation in binary_tree_size
    Updated the parameter documentation for clarity.

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,15 +2,15 @@
 
 /**
  * binary_tree_size - measures the size of a binary tree
- * @tree: pointer to the root node
+ * @tree: pointer to the root node of the tree
  *
  * Return: size of the tree
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    return (1 + binary_tree_size(tree->left) +
-                binary_tree_size(tree->right));
+	return (1 + binary_tree_size(tree->left)
+		+ binary_tree_size(tree->right));
 }
